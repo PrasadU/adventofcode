c4e1278fb10aa1661fb94406e9d141f593d3ea63
Prasad U <uvbmpras@gmail.com>
2023-12-05 19:03:02 +0000

    part 1 done

--- 2023/1205/solution1.py
+++ 2023/1205/solution1.py
@@ -5,7 +5,7 @@ def read_almanac(file_name):
     lines = open(file_name).readlines()
     seeds = get_seeds(lines[0])
     map_names = []
-    map_of_maps = {}
+    map_of_lists = {}
     last_map_name = ''
     for i in range(1, len(lines)):
         cur_line = lines[i].strip()
@@ -15,12 +15,11 @@ def read_almanac(file_name):
             map_name = re.match(r'(\S+)\s+map:', cur_line).group(1)
             last_map_name = map_name
             map_names.append(map_name)
-            map_of_maps[map_name] = {}
+            map_of_lists[map_name] = []
         else:
             # read line
             raw_def = list(map(str_to_int, re.match(r'(\d+) +(\d+) +(\d+)', cur_line).groups()))
-            for ri in range(raw_def[2]):
-                map_of_maps[last_map_name][raw_def[1] + ri] = raw_def[0] + ri
+            map_of_lists[last_map_name].append((raw_def[1], raw_def[0], raw_def[2]))
     worked_paths = {}
     min_location = None
     min_sid = None
@@ -28,14 +27,23 @@ def read_almanac(file_name):
         worked_paths[sid] = [sid]
         last_lookup = sid
         for mn in map_names:
-            last_lookup = map_of_maps[mn].setdefault(last_lookup, last_lookup)
+            last_lookup = lookup_next(map_of_lists[mn], last_lookup)
             worked_paths[sid].append(last_lookup)
         if min_location is None or last_lookup < min_location:
             min_location = last_lookup
             min_sid = sid
         print(f'{min_sid} with {min_location} : {sid} : {worked_paths[sid]}')
 
-    return min_location, {'seeds': seeds, 'map_of_maps': map_of_maps, 'worked_paths': worked_paths}
+    return min_location, {'seeds': seeds, 'map_of_maps': map_of_lists, 'worked_paths': worked_paths}
+
+
+def lookup_next(rules_list, key):
+    key_value = key
+    for rule in rules_list:
+        if rule[0] <= key < rule[0]+rule[2]:
+            key_value = rule[1] + key - rule[0]
+            break
+    return key_value
 
 
 def get_seeds(line) -> [str]:
--- 2023/1205/test.py
+++ 2023/1205/test.py
@@ -1,8 +1,31 @@
-from solution import read_almanac
+from solution1 import read_almanac
+
+
+def test_input():
+    read_almanac('input.txt')
 
 
 def test_example():
     almanac = read_almanac('example.txt')
-    assert almanac[0]['seeds'] == [79, 14, 55, 13]
-    assert almanac[1] == 35
-    
+    expected_seeds = [79, 14, 55, 13]
+    expected_paths = {
+        79: [79, 81, 81, 81, 74, 78, 78, 82],
+        14: [14, 14, 53, 49, 42, 42, 43, 43],
+        55: [55, 57, 57, 53, 46, 82, 82, 86],
+        13: [13, 13, 52, 41, 34, 34, 35, 35],
+    }
+
+    assert almanac[1]['seeds'] == expected_seeds
+    # seed_to_soil = almanac[1]['map_of_maps']['seed-to-soil']
+    # for i in range(49):
+    #     assert seed_to_soil.setdefault(i, i) == i
+    # for i in range(50, 98):
+    #     assert seed_to_soil.setdefault(i, i) == 52 + (i-50)
+    # assert seed_to_soil.setdefault(98, 98) == 50
+    # assert seed_to_soil.setdefault(99, 99) == 51
+    # for i in range(100, 110):
+    #     assert seed_to_soil.setdefault(i, i) == i
+    for sid in expected_seeds:
+        assert almanac[1]['worked_paths'][sid] == expected_paths[sid]
+    assert almanac[0] == 35
+
