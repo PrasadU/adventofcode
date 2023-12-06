de20c0721e123969585306879364dd4632b714b3
Prasad U <uvbmpras@gmail.com>
2023-12-06 06:44:26 +0000

    part 2 timed out

--- 2023/1205/solution1.py
+++ 2023/1205/solution1.py
@@ -1,9 +1,8 @@
 import regex as re
-
+import threading
 
 def read_almanac(file_name):
     lines = open(file_name).readlines()
-    seeds = get_seeds(lines[0])
     map_names = []
     map_of_lists = {}
     last_map_name = ''
@@ -20,21 +19,42 @@ def read_almanac(file_name):
             # read line
             raw_def = list(map(str_to_int, re.match(r'(\d+) +(\d+) +(\d+)', cur_line).groups()))
             map_of_lists[last_map_name].append((raw_def[1], raw_def[0], raw_def[2]))
-    worked_paths = {}
+    # maps read - loop seeds
+    seed_numbers = get_seeds_numbers(lines[0])
+    threads = []
+    sni = 0
+    while sni <= len(seed_numbers)-2:
+        start = seed_numbers[sni]
+        end = start + seed_numbers[sni+1]
+        print(f'running {seed_numbers[sni]} ~ {seed_numbers[sni+1]}')
+        thread = ProcThread(map_names, map_of_lists, start, end)
+        threads.append(thread)
+        thread.start()
+        sni += 2
+    for thread in threads:
+        thread.join()
+
+    min_sid = None
+    min_location = None
+    for batch in threads:
+        if min_location is None or batch.min[0] < min_location:
+            min_location = batch.min[0]
+            min_sid = batch.min[1]
+    print(f'{min_sid} with {min_location}')
+    return min_location, {'seeds': [], 'map_of_maps': map_of_lists, 'worked_paths': []}
+
+
+def find_min_in_a_set(map_names, map_of_lists, start, end):
     min_location = None
     min_sid = None
-    for sid in seeds:
-        worked_paths[sid] = [sid]
+    for sid in range(start, end):
         last_lookup = sid
         for mn in map_names:
             last_lookup = lookup_next(map_of_lists[mn], last_lookup)
-            worked_paths[sid].append(last_lookup)
         if min_location is None or last_lookup < min_location:
             min_location = last_lookup
             min_sid = sid
-        print(f'{min_sid} with {min_location} : {sid} : {worked_paths[sid]}')
-
-    return min_location, {'seeds': seeds, 'map_of_maps': map_of_lists, 'worked_paths': worked_paths}
+    return min_location, min_sid
 
 
 def lookup_next(rules_list, key):
@@ -46,15 +66,28 @@ def lookup_next(rules_list, key):
     return key_value
 
 
-def get_seeds(line) -> [str]:
-    seeds = []
+def get_seeds_numbers(line) -> [str]:
+    seed_numbers = []
     m_itr = re.finditer(r'seeds:( (\d+))*', line)
     if m_itr:
         for m in m_itr:
-            seeds = list(map(str_to_int, m.captures(2)))
-    print(f'seeds: {seeds}')
-    return seeds
+            seed_numbers = list(map(str_to_int, m.captures(2)))
+    print(f'seed_numbers: {seed_numbers}')
+    return seed_numbers
 
 
 def str_to_int(str_val) -> int:
     return int(str_val)
+
+
+class ProcThread(threading.Thread):
+    def __init__(self, map_names, map_of_lists, begin, end):
+        super(ProcThread, self).__init__()
+        self.map_names = map_names
+        self.map_of_lists = map_of_lists
+        self.begin = begin
+        self.end = end
+        self.min = (None, None)
+
+    def run(self):
+        self.min = find_min_in_a_set(self.map_names, self.map_of_lists, self.begin, self.end)
--- 2023/1205/test.py
+++ 2023/1205/test.py
@@ -7,15 +7,13 @@ def test_input():
 
 def test_example():
     almanac = read_almanac('example.txt')
-    expected_seeds = [79, 14, 55, 13]
+    expected_seeds = [79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67]
     expected_paths = {
         79: [79, 81, 81, 81, 74, 78, 78, 82],
-        14: [14, 14, 53, 49, 42, 42, 43, 43],
         55: [55, 57, 57, 53, 46, 82, 82, 86],
-        13: [13, 13, 52, 41, 34, 34, 35, 35],
     }
 
-    assert almanac[1]['seeds'] == expected_seeds
+    # assert almanac[1]['seeds'] == expected_seeds
     # seed_to_soil = almanac[1]['map_of_maps']['seed-to-soil']
     # for i in range(49):
     #     assert seed_to_soil.setdefault(i, i) == i
@@ -25,7 +23,7 @@ def test_example():
     # assert seed_to_soil.setdefault(99, 99) == 51
     # for i in range(100, 110):
     #     assert seed_to_soil.setdefault(i, i) == i
-    for sid in expected_seeds:
-        assert almanac[1]['worked_paths'][sid] == expected_paths[sid]
-    assert almanac[0] == 35
+    # for sid in expected_paths.keys():
+    #     assert almanac[1]['worked_paths'][sid] == expected_paths[sid]
+    assert almanac[0] == 46
 
